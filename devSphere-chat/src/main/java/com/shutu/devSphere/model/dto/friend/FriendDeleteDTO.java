a7b12dc51c91c215ebbb4a74cb90a9a3b378431d
shutu <3321652135@qq.com>
2025-11-14 20:31:42 +0800

    feat:好友管理

---
+++ devSphere-chat/src/main/java/com/shutu/devSphere/model/dto/friend/FriendDeleteDTO.java
@@ -0,0 +1,14 @@
+package com.shutu.devSphere.model.dto.friend;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Schema(description = "删除好友DTO")
+public class FriendDeleteDTO {
+
+    @Schema(description = "好友的用户ID", required = true)
+    @NotNull(message = "好友ID不能为空")
+    private Long friendId;
+}
